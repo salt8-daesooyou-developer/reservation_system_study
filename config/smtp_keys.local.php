8dd55bd41a0ca04ac8dccc03a68d651304dc4634
salt8-daesooyou-developer <daesoo.you@salteight.com>
2026-08-10 17:15:55 +0900

    Create smtp_keys.local.php

---
+++ config/smtp_keys.local.php
@@ -0,0 +1,13 @@
+<?php
+// ローカル専用の SMTP 設定（.gitignore 対象、コミットされません）。
+// SMTP利用が許可されたら、下の空欄に入力してください。
+return [
+    'host' => '',
+    'port' => 587,
+    'secure' => 'tls',
+    'username' => '',
+    'password' => '',
+    'from_email' => '',
+    'from_name' => 'RSVP',
+    'to_email' => '',
+];
